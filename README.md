9b0cb758e8784a10981df46ba611ea4a5a5c6c47
happychen666 <2695401775@qq.com>
2020-01-14 09:53:35 +0800

    搭建项目骨架

---
+++ README.md
@@ -0,0 +1,9 @@
+# React新特性Hooks写类似买票软件
+
+该项目使用了 [React](https://reactjs.org)、[Redux](https://redux.js.org/)等技术，重度使用 [Hooks](https://reactjs.org/docs/hooks-intro.html) 重写视图组件。
+
+## 用法
+
+```sh
+npm start
+```
---
+++ src/query/App.jsx
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import './App.css';
+
+function App(props){
+
+}
+
+const mapStateToProps = (state)=({
+
+})
+
+const mapDispatchToProps = (dispatch) => {
+    
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
