947b507208a3c3ddbf890e784b0c443a30670a74
happychen666 <2695401775@qq.com>
2020-01-14 12:47:58 +0800

    完成header组件

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, { useCallback } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 import Header from '../common/header/Header.jsx';
@@ -8,23 +8,30 @@ import HighSpeed from './highSpeed/HighSpeed';
 import Journery from './journery/Journery';
 import Submit from './submit/Submit.jsx';
 
-function App(props){
+function App(props) {
+
+    //向子组件传入函数，为了避免子组件每次重渲染用useCallback将函数包裹起来
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, [])
     return (
         <div>
-            <Header/>
-            <Journery/>
-            <DepartDate/>
-            <HighSpeed/>
-            <Submit/>
+            <div className="header-wrapper">
+                <Header title="火车票" onBack={onBack} />
+            </div>
+            <Journery />
+            <DepartDate />
+            <HighSpeed />
+            <Submit />
         </div>
     )
 }
 
-const mapStateToProps = (state)=>({
+const mapStateToProps = (state) => ({
 
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    
+
 })
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
