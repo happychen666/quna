69362db8359522af97dc579f46901ca4264da903
happychen666 <2695401775@qq.com>
2020-01-14 13:57:38 +0800

    起始终止站基本搭建

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -5,33 +5,59 @@ import './App.css';
 import Header from '../common/header/Header.jsx';
 import DepartDate from './departdate/DepartDate';
 import HighSpeed from './highSpeed/HighSpeed';
-import Journery from './journery/Journery';
+import Journey from './journey/Journey';
 import Submit from './submit/Submit.jsx';
 
+import {
+    exchangeFromTo,
+    showCitySelector
+} from './store/actions';
 function App(props) {
+
+    const {
+        from,
+        to,
+        dispatch
+    } = props;
 
     //向子组件传入函数，为了避免子组件每次重渲染用useCallback将函数包裹起来
     const onBack = useCallback(() => {
         window.history.back();
+    }, []);
+
+    const doExchangeFromTo = useCallback(() => {
+        dispatch(exchangeFromTo());
+    }, []);
+
+    const doShowCitySelector = useCallback((m) => {
+        dispatch(showCitySelector(m));
     }, [])
+
     return (
         <div>
             <div className="header-wrapper">
                 <Header title="火车票" onBack={onBack} />
             </div>
-            <Journery />
-            <DepartDate />
-            <HighSpeed />
-            <Submit />
+            <form className="form">
+                <Journey
+                    from={from}
+                    to={to}
+                    showCitySelector={doShowCitySelector}
+                    exchangeFromTo={doExchangeFromTo}
+                />
+                <DepartDate />
+                <HighSpeed />
+                <Submit />
+            </form>
         </div>
     )
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => {
+    return state;
+}
 
-})
-
-const mapDispatchToProps = (dispatch) => ({
-
-})
+const mapDispatchToProps = (dispatch) => {
+    return { dispatch }
+}
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index/journery/Journery.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Journery.css';
-
-export default function Journery(props){
-    return (
-        <div></div>
-    )
-}
